c7ef25a858886ef4bd966bd3d9e2c687f69e6e5e
jay <cleve703@gmail.com>
2019-07-23 15:38:15 -0400

    Added 5 round games.

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
 var computerSelection;
 var playerSelection;
+var outcome;
+var countTie;
+var countWin;
+var countLoss;
 
-playRound()
+playGame()
 
-function playRound() {
-    playerSelection = prompt("Choose paper, rock, or scissors:")
-    playerSelection = playerSelection.toLowerCase()
-    // console.log(playerSelection)
-    computerPlay()
-    // console.log(computerSelection)
-}
+function playGame() {
+    var i;
+    countTie = 0
+    countWin = 0
+    countLoss = 0
+    for (i = 0; i < 5; i++) {
+    playRound()
+    console.log(`You played ${playerSelection} and the computer played ${computerSelection}.`)
+    whoWon()
+    if (outcome == "tie") {
+        countTie++
+        console.log("You tied.")
+    }
+    else if (outcome == "win") {
+        countWin++
+        console.log("You won!")
+    }
+    else if (outcome == "loss") {
+        countLoss++
+        console.log("You lost.")
+    }
+    }
+    console.log(`Your total wins is: ${countWin}.\nYour total losses is ${countLoss}.\nYour total games tied is ${countTie}.`)
+};
 
-function computerPlay() {
-    var min = 1;
-    var max = 4;
-    var numericSelection = (Math.floor(Math.random() * (+max - +min)) + +min);
-        if (numericSelection == 1) {computerSelection = "rock"}
-        else if (numericSelection == 2) {computerSelection = "paper"}
-        else if (numericSelection == 3) {computerSelection = "scissors"};
-    return computerSelection
-}
+    function playRound() {
+            playerSelection = prompt("Choose paper, rock, or scissors:")
+            playerSelection = playerSelection.toLowerCase()
+            // console.log(playerSelection)
+            computerPlay()
+            // console.log(computerSelection)
+    }
 
-if (playerSelection == computerSelection) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  Tie!`)
-else if ((playerSelection == 'rock') && (computerSelection == 'scissors')) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  You win.`)
-else if ((playerSelection == 'rock') && (computerSelection == 'paper')) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  You lose.`)
-else if ((playerSelection == 'paper') && (computerSelection == 'rock')) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  You win.`)
-else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  You lose.`)
-else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  You lose.`)
-else if ((playerSelection == 'scissors') && (computerSelection == 'paper')) console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}.  You win.`)
+    function computerPlay() {
+        var min = 1;
+        var max = 4;
+        var numericSelection = (Math.floor(Math.random() * (+max - +min)) + +min);
+            if (numericSelection == 1) {computerSelection = "rock"}
+            else if (numericSelection == 2) {computerSelection = "paper"}
+            else if (numericSelection == 3) {computerSelection = "scissors"};
+        return computerSelection
+    }
+
+function whoWon() {
+    if (playerSelection == computerSelection) {outcome = "tie"} 
+    else if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {outcome = "win"}
+    else if ((playerSelection == 'rock') && (computerSelection == 'paper')) {outcome = "loss"}
+    else if ((playerSelection == 'paper') && (computerSelection == 'rock')) {outcome = "win"}
+    else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {outcome = "loss"}
+    else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {outcome = "loss"}
+    else if ((playerSelection == 'scissors') && (computerSelection == 'paper')) {outcome = "win"}
+};
